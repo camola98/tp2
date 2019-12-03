6dc4b0680259a66235e268d56e78317ec35917f8
camola98 <ccarlocchia@fi.uba.ar>
2019-12-03 19:51:12 -0300

    empece procesador csv, vuelos crear

--- tp2.c
+++ tp2.c
@@ -26,9 +26,61 @@ de ser ejecutado. Si un comando no pertenece a los listados previamente o
 tiene un error, se imprime Error en comando <comando> por stderr y 
 continua la ejecución.
 */
-typedef struct vuelos{
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct vuelo{
        char* codigo;
+       char* aerolinea;
+       char* aeropuerto_origen;
+       char* aeropuerto_destino;
+       char* tail_number;
        size_t prioridad;
-       size_t horario;
-       size_t fecha;
-}vuelos_t;
+       char* tiempo;
+       size_t* air_time;
+       size_t* cancelled;
+}vuelo_t;
+
+vuelo_t* vuelo_crear(char** info){
+    vuelo_t* flight = malloc(sizeof(vuelo_t));
+    if (!flight) return NULL;
+    flight->codigo = info[0];
+    flight->aerolinea = info[1];
+    flight->aeropuerto_origen = info[2];
+    flight->aeropuerto_destino = info[3];
+    flight->tail_number = info[4];
+    flight->prioridad = *(size_t*)info[5];
+    flight->tiempo = info[6];
+    flight->air_time = *(size_t*)info[7];
+    flight->cancelled = *(size_t*)info[8];
+    return flight;
+}
+
+//procesar el csv
+/*ej. formato
+    FLIGHT_NUMBER: 4608
+    AIRLINE: OO
+    ORIGIN_AIRPORT: PDX
+    DESTINATION_AIRPORT: SEA
+    TAIL_NUMBER: N812SK
+    PRIORITY: 08
+    DATE: 2018-04-10T23:22:55
+    DEPARTURE_DELAY: 05
+    AIR_TIME: 43
+    CANCELLED: 0
+*/
+vuelo_t** lector_archivo(char* file_name, char* comando ){
+    FILE* file = fopen(file_name, "r");
+    if (!file){
+        fprintf( stderr, "Error en comando < %s >", comando);
+        return;
+        }
+    char* linea = NULL;
+    size_t capacidad = 0;
+    while (getline(&linea, &capacidad, file) != -1){
+        char** arreglo = split(linea, ',');
+        vuelo_t* vuelo = vuelo_crear(arreglo);
+    }
+}
